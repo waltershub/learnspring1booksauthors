aac186a7e42d37b1aff1a200124435ab926002e5
Walter Shub <waltershub@gmail.com>
2019-12-15 00:02:29 -0500

    first commit

---
+++ src/test/java/walter/springframework/spring5webapp/Spring5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package walter.springframework.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
